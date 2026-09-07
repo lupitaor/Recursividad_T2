a268bc6d62ae007cd3d626dad9b4f8f317c026b9
lupitaor <amartinez@itesa.edu.mx>
2026-09-07 08:48:19 -0600

    Versión_02: Factorial recursivo.

---
+++ Recursividad.Logica/Factorial.cs
@@ -0,0 +1,21 @@
+﻿namespace Recursividad.Logica
+{
+    public class Factorial
+    {
+        public int MiFactorial(int numero)
+        {
+            //Caso base o condición de salida
+            if (numero == 1)
+            {
+                return 1;
+            }
+
+            //Caso genera
+
+            return numero = numero * MiFactorial(numero - 1);
+        }
+
+
+
+    }
+}
---
+++ Recursividad.UI.Consola/Program.cs
@@ -0,0 +1,13 @@
+﻿using Recursividad.Logica;
+
+Console.WriteLine("Hello, World!");
+
+Factorial oFactorial = new Factorial();
+
+
+int resultado = oFactorial.MiFactorial(5);
+
+Console.WriteLine("Factorial de 5: " + resultado);
+
+
+
